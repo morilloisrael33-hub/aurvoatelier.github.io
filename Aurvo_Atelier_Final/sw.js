3e8ae1783694105762215465a5ce0bcd3b9122a7
albertomorillo <morilloisrael33@gmail.com>
2025-12-22 11:39:35 -0500

    Add files via upload

---
+++ Aurvo_Atelier_Final/sw.js
@@ -0,0 +1,5 @@
+﻿const CACHE_NAME = 'aurvo-v10-final';
+const ASSETS = ['./', './index.html', './style.css'];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
